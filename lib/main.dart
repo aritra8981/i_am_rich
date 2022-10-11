9071502c743a77e10785414edfa9ea2caa73175b
Aritra Das <aritradas00@gmail.com>
2022-10-11 16:13:38 +0530

    Updated Version

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ void main() {
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'),
+            image: AssetImage('images/diamond.png'),
+            // image: NetworkImage('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'),
           ),
         ),
         backgroundColor: Colors.blue,
